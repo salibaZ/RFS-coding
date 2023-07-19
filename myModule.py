78dccc7279ed48381f9fe7e44e30ed7a90bc7315
salibaZ <139952428+salibaZ@users.noreply.github.com>
2023-07-19 10:07:50 +0300

    Add files via upload

---
+++ myModule.py
@@ -0,0 +1,14 @@
+# myModule.py
+# we can import this or any other Python file we wrote
+
+# myModule.dozen -> 12
+dozen = 12
+
+# myModule.greeting()
+def greeting():
+    print("Hello world!")
+
+# function to square a number
+# myModule.square(...)
+def square(number):
+    return number * number
